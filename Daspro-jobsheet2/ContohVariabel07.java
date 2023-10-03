178f596835fd4b122ab4c2dcbed9c8109688ab4d
desikaaa <143399917+desikaaa@users.noreply.github.com>
2023-10-03 17:11:20 +0700

    Add files via upload

---
+++ Daspro-jobsheet2/ContohVariabel07.java
@@ -0,0 +1,15 @@
+public class ContohVariabel07 {
+
+    public static void main (String [] args){
+        String salahSatuHobySayaAdalah = "Bersepeda";
+        boolean isPandai = true;
+        char jenisKelamin ='P';
+        byte _umurSayaSekarang = 19;
+        double $ipk = 3.94, tinggi = 1.65;
+        System.out.println(salahSatuHobySayaAdalah);
+        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("Jenis kelamin: " + jenisKelamin);
+        System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+    }
+}
